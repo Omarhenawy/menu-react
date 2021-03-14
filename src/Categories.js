7388718a457ca20978c126eeaad184e9e37dd871
Omar Henawy <omarhenawy31@gmail.com>
2021-03-14 15:22:29 +0200

    first commit

---
+++ src/Categories.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Categories = ({filterItems}) => {
+  return (
+    <div className='btn-container'>
+      <button 
+      className='filter-btn'
+      onClick={()=> filterItems('all')}>
+        All
+
+      </button>
+      <button 
+      className='filter-btn'
+      onClick={()=> filterItems('breakfast')}>
+        breakfast
+
+      </button>
+      <button 
+      className='filter-btn'
+      onClick={()=> filterItems('lunch')}>
+        lunch
+      </button>
+            <button 
+      className='filter-btn'
+      onClick={()=> filterItems('shakes')}>
+        shakes
+      </button>
+
+
+
+
+    </div>
+  );
+};
+
+export default Categories;
